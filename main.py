4b6451a933abcfac6ddd7f2f7815d2f1864a26ce
tycoder1 <tyty122704@gmail.com>
2020-07-30 15:13:58 +0000

    Added movement function and scaling code

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class character(object):
             pygame.sprite.Sprite.__init__(self)
             character.image = pygame.image.load("chell.png")
             self.image = character.image
+            self.image = pygame.transform.scale(self.image,(250,200))
 
             self.x = 50
             self.y = 50
@@ -18,7 +19,17 @@ class character(object):
             surface.blit(self.image,(self.x,self.y))
             def __init__(self):
                 pygame.sprite.Sprite.__init__(self)
-
+                
+        def movement(self):
+            key = pygame.key.get_pressed()
+            if key[pygame.K_LEFT]:
+               self.x -= 1
+               if key[pygame.K_RIGHT]:
+                   self.x += 1
+                   if key[pygame.K_UP]:
+                       self.y -= 1
+                       if key[pygame.K_DOWN]:
+                           self.y += 1
 pygame.init()
 screen_width = 600
 screen_hieght = 600
@@ -29,24 +40,20 @@ clock = pygame.time.Clock()
 
 running = True
 while running:
-                # handle every event since the last frame.
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() # quit the screen
-            running = False
+     if event.type == pygame.QUIT:
+        pygame.quit() # quit the screen
+        running = False
         screen.fill((255,255,255))
+        
+        character.movement(Sprite)
+        
         Sprite.draw(screen)
-
+     
         pygame.display.update()
         
         clock.tick(60)
 
-def movement():
-     if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT or event.key == ord('a'):
-                print('left')
-        if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                print('right')
-        if event.key == pygame.K_UP or event.key == ord('w'):
-            print('jump')
-        
+
+
+     
